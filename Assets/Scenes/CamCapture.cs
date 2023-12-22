51d5c7a3fa63a4d33d6254cca9ed496d86eb5fdf
hiep21 <nguyentrunghiep2102@gmail.com>
2023-12-22 15:23:17 +0700

    Update Asset Cam Capture Creep

--- Assets/Scenes/CamCapture.cs
+++ Assets/Scenes/CamCapture.cs
@@ -4,15 +4,41 @@ using System.IO;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using TMPro;
+using Spine.Unity;
 namespace Rubik.Axie
 {
     public class CamCapture : MonoBehaviour
     {
         public string NamePNG = "default";
+        public SkeletonAnimation skeletonAnimation;
 
+        void Update()
+        {
+            NamePNG = skeletonAnimation.initialSkinName.Replace("Pet", "");
+        }
         public TMP_InputField txtName;
+        // [Button]
+        // public void Capture()
+        // {
+        //     Camera Cam = GetComponent<Camera>();
+
+        //     RenderTexture currentRT = RenderTexture.active;
+        //     RenderTexture.active = Cam.targetTexture;
+
+        //     Cam.Render();
+
+        //     Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        //     Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        //     Image.Apply();
+        //     RenderTexture.active = currentRT;
+
+        //     var Bytes = Image.EncodeToPNG();
+        //     Destroy(Image);
+
+        //     File.WriteAllBytes(Application.dataPath + "/CamCapture/" + NamePNG + ".png", Bytes);
+        // }
         [Button]
-        public void Capture()
+        public void CaptureCreep()
         {
             Camera Cam = GetComponent<Camera>();
 
@@ -29,8 +55,28 @@ namespace Rubik.Axie
             var Bytes = Image.EncodeToPNG();
             Destroy(Image);
 
-            File.WriteAllBytes(Application.dataPath + "/CamCapture/"+NamePNG+".png", Bytes);
+            File.WriteAllBytes(Application.dataPath + "/CamCapture/Creeps/" + NamePNG + ".png", Bytes);
         }
+        // [Button]
+        // public void CaptureBoss()
+        // {
+        //     Camera Cam = GetComponent<Camera>();
+
+        //     RenderTexture currentRT = RenderTexture.active;
+        //     RenderTexture.active = Cam.targetTexture;
+
+        //     Cam.Render();
+
+        //     Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        //     Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        //     Image.Apply();
+        //     RenderTexture.active = currentRT;
+
+        //     var Bytes = Image.EncodeToPNG();
+        //     Destroy(Image);
+
+        //     File.WriteAllBytes(Application.dataPath + "/CamCapture/Creeps/Boss/" + NamePNG + ".png", Bytes);
+        // }
 
         public void CapturePic()
         {
