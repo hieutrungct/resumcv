605348fdf576af6b0d24e18e383b2f7f72be14c5
hieutrungct <106019570+hieutrungct@users.noreply.github.com>
2024-04-12 18:37:15 +0700

    Update GamePlay1

---
+++ Assets/_Rubik-Casual/GamePlay1/Scripts/CardWaifu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using RubikCasual.Data.Player;
+using UnityEngine;
+namespace RubikCasual.ListWaifu
+{
+    public class CardWaifu : MonoBehaviour
+    {
+        public void SetUp(PlayerOwnsWaifu waifu)
+        {
+            
+        }
+    }
+}
+
