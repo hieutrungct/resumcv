63eb5c15f91a964bef6fc3701d89268ad312a824
hieutrungct <106019570+hieutrungct@users.noreply.github.com>
2024-06-10 18:20:30 +0700

    update ListWaiffuController

--- Assets/_Rubik-Casual/GamePlay1/Scripts/ListWaifu/ListWaifuController.cs
+++ Assets/_Rubik-Casual/GamePlay1/Scripts/ListWaifu/ListWaifuController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Rubik.ListWaifu;
 using RubikCasual.Data;
 using RubikCasual.Data.Player;
@@ -14,32 +15,34 @@ namespace RubikCasual.ListWaifu
         public CardWaifu slot_card;
         public Transform contentWaifu;
         public List<PlayerOwnsWaifu> lsWaifu;
+        public List<CardWaifu> lsInfoCardClone;
+        public List<Transform> lsSlot;
+        public Transform posInstantiateCard;
         void Awake()
         {
             instance = this;
-            lsWaifu = DataController.instance.playerData.lsPlayerOwnsWaifu;
+            // lsWaifu = DataController.instance.playerData.lsPlayerOwnsWaifu;
             // SortRarityAndLevel();
         }
-        
         public void SetUpListWaifu()
         {
-
-            // for (int i = 0; i < DataController.instance.playerData.lsPlayerOwnsWaifu.Count; i++)
-            // {
-            //     CardWaifu cardWaifu = Instantiate(slot_card, contentWaifu);
-            //     cardWaifu.SetUp(DataController.instance.playerData.lsPlayerOwnsWaifu[i]);
-            // }
-            foreach (Transform child in contentWaifu)
+            foreach (Transform child in posInstantiateCard)
             {
                 Destroy(child.gameObject);
             }
             for (int i = lsWaifu.Count - 1; i > -1; i--)
             {
-                CardWaifu slotWaifu = Instantiate(slot_card, contentWaifu);
+                CardWaifu slotWaifu = Instantiate(slot_card, posInstantiateCard);
                 slotWaifu.SetUp(lsWaifu[i]);
+                lsInfoCardClone.Add(slotWaifu);
             }
         }
         [Button]
+        public void LoadScene()
+        {
+            lsWaifu = DataController.instance.playerData.lsPlayerOwnsWaifu;
+            SortRarityAndLevel();
+        }
         public void SortRarityAndLevel()
         {
             
@@ -58,9 +61,18 @@ namespace RubikCasual.ListWaifu
             SetUpListWaifu();
         }
 
-
-
-        
+        public void ScaleCard(int index)
+        {
+            CardWaifu infoCardClone = lsInfoCardClone[index];
+            infoCardClone.transform.DOScale(new Vector3(1f, 1f, 1f), 0.25f);
+            infoCardClone.transform.DOMove(lsSlot[index].position, 0.25f)
+            .OnComplete(() =>
+            {
+                infoCardClone.transform.DOJump(infoCardClone.transform.position, 1f / 3f, 1, 0.5f);
+                
+            });
+            index++;
+        }
     }
 }
 
